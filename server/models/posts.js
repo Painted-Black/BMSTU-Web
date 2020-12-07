8a18d6c7833e35cb29a9952a6ef0198b8459c1b3
Painted-Black <syzygy.sys@gmail.com>
2020-12-07 20:44:24 +0300

    Small fixes

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,31 +1,34 @@
-const Sequelize = require('sequelize'); 
+const Sequelize = require('sequelize');
 const sequelize = require('../db'); // импорт подключения
 
 // Определяем модель постов
 const Posts = sequelize.define('posts', {
-	id: {
-		primaryKey: true,
-		autoIncrement: true,
-		type: Sequelize.INTEGER
-	},
-	title: {
-		type: Sequelize.STRING(140)
-	},
-	slug: {
-		type: Sequelize.STRING(140),
-		unique: true
-	},
-	body: {
-		type: Sequelize.TEXT
-	},
-	created: {
-		type: Sequelize.DATE,
-		defaultValue: Sequelize.NOW
-	}
-},
-{
-	timestamps: false, // чтобы sequelize не запрашивал дефолтные поля createdAt и updatedAt
-	tableName: 'posts'
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+    },
+    title: {
+        type: Sequelize.STRING(140),
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING(140),
+        unique: true,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    created: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+    }
+}, {
+    timestamps: false, // чтобы sequelize не запрашивал дефолтные поля createdAt и updatedAt
+    tableName: 'posts'
 })
 
 module.exports = Posts;
--- server/models/tags.js
+++ server/models/tags.js
@@ -1,22 +1,23 @@
-const Sequelize = require('sequelize'); 
+const Sequelize = require('sequelize');
 const sequelize = require('../db'); // импорт подключения
 
 const Tags = sequelize.define('tags', {
-	id: {
-		primaryKey: true,
-		autoIncrement: true,
-		type: Sequelize.INTEGER
-	},
-	name: {
-		type: Sequelize.STRING(100)
-	},
-	slug: {
-		type: Sequelize.STRING(100)
-	}
-},
-{
-	timestamps: false, // чтобы sequelize не запрашивал дефолтные поля createdAt и updatedAt
-	tableName: 'tags'
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+    },
+    name: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+    }
+}, {
+    timestamps: false, // чтобы sequelize не запрашивал дефолтные поля createdAt и updatedAt
+    tableName: 'tags'
 })
 
 module.exports = Tags;
